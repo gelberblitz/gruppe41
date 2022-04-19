bbc9e840bea7c8358365cbe43b71445a508d65d3
Sebastian <sebastian.froehlich@hhu.de>
2022-04-20 01:28:34 +0200

    Finished

--- Test.java
+++ Test.java
@@ -3,7 +3,10 @@ public class Atakan{
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+	    System.out.println("!"); 
         
+        System.out.println(x);
+
     }
 
 }
